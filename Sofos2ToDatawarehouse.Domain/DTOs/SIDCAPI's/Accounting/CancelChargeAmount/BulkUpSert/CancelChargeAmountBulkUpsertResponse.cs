1576c0b8f24efa1a22d5fcac84e7bfc753f2de75
Cardo <degracia.arjay@gmail.com>
2025-06-17 16:36:40 +0800

    Sender CancelChargeAmount Transferred

--- Sofos2ToDatawarehouse.Domain/DTOs/SIDCAPI's/Accounting/CancelChargeAmount/BulkUpSert/CancelChargeAmountBulkUpsertResponse.cs
+++ Sofos2ToDatawarehouse.Domain/DTOs/SIDCAPI's/Accounting/CancelChargeAmount/BulkUpSert/CancelChargeAmountBulkUpsertResponse.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Sofos2ToDatawarehouse.Domain.DTOs.SIDCAPI_s.Accounting.CancelChargeAmount.BulkUpSert
 {
-    public class CancelChargeAmountBulkUpsertResponse : SIDCBaseResponse
+    public class CancelChargeAmountBulkUpsertResponse 
     {
-        public string Data { get; set; }
+        public ApiResponse response { get; set; }
+
+        public bool Succeeded => response?.status_code == 200;
+    }
+    public class ApiResponse
+    {
+        public int status_code { get; set; }
+        public Result result { get; set; }
+    }
+
+    public class Result
+    {
+        public string message { get; set; }
     }
 }
